7296a168b4f1eb28c1e4b91d469891a12873876a
Istvan Gere <istvan.gere.89@gmail.com>
2023-09-12 21:54:45 +0200

    initial spring app

---
+++ src/main/java/hu/igr/igrline/IgrlineBeJavaApplication.java
@@ -0,0 +1,13 @@
+package hu.igr.igrline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrlineBeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrlineBeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/igr/igrline/IgrlineBeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.igr.igrline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrlineBeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
